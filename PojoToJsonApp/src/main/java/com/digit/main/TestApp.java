bd7a00118390a0bb06afa275e506e58f7c27e4b2
FaizAhmad109 <faizahmad11109@gmail.com>
2023-11-14 00:15:43 +0530

    uat files

--- PojoToJsonApp/src/main/java/com/digit/main/TestApp.java
+++ PojoToJsonApp/src/main/java/com/digit/main/TestApp.java
@@ -1,27 +1,26 @@
 package com.digit.main;
 
-import java.io.File;
-import java.io.IOException;
-
-import com.digit.model.Customer;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.digit.model.Product;
 
 public class TestApp {
 
 	public static void main(String[] args) {
 
 		try {
-			// create objectMapper
-			ObjectMapper mapper = new ObjectMapper();
-			// using that object read the data from json file and convert it into java
-			// object
-			Customer customer = mapper.readValue(new File("data/sample-full.json"), Customer.class);
-			System.out.println(customer);
-		} catch (IOException e) {
+			
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+	}
+	
+	public static Product getObjectData(Product product)
+	{
+		product.setPid(11);
+		product.setPname("Faiz");
+		product.setPrice(28900.0);
+		return product;
 	}
 
 }
---
+++ PojoToJsonApp/src/main/java/com/digit/model/Product.java
@@ -0,0 +1,11 @@
+package com.digit.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+	private Integer pid;
+	private String pname;
+	private Double price;
+}
