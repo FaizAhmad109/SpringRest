d76b7b41d32f6a9bfd9a2a1a6fd8755fb02952c1
FaizAhmad109 <faizahmad11109@gmail.com>
2023-11-14 00:58:24 +0530

    all changes to uat

--- PojoToJsonApp/src/main/java/com/digit/main/TestApp.java
+++ PojoToJsonApp/src/main/java/com/digit/main/TestApp.java
@@ -1,13 +1,20 @@
 package com.digit.main;
 
 import com.digit.model.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestApp {
 
 	public static void main(String[] args) {
 
 		try {
-			
+			//create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+			Product product = new Product();
+			product=getObjectData(product);
+			//converting java object into json string
+			String jsonData = mapper.writeValueAsString(product);
+			System.out.println(jsonData);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
